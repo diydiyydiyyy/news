6c1c00c6f070dae08f022f3a60dfa0e2a01e760c
diydiyydiyyy <chalimatus.sadiyah.098@gmail.com>
2022-06-22 13:00:23 +0700

    base project

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#44555F",
+        secondary: "#C3D54B",
+        light: "#FFFFFD",
+        dark: "#161B22",
+      },
+      fontFamily: {
+        lato: ["Lato"],
+      },
+    },
+  },
+  plugins: [],
+}
